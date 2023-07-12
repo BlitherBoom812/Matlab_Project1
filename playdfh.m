659f5cee5e5b767ccb6fb5711dc2187904b42396
Shizuo <tsz21@mails.tsinghua.edu.cn>
2023-07-12 14:49:21 +0000

    update expo_envelop

--- playdfh.m
+++ playdfh.m
@@ -41,3 +41,24 @@ function [freq, width] = trans_freq_width(tone, beat)
     freq = base_tone_freq * (2^((exponent - 1)/12));
     width = beat * beat_time;
 end
+
+function [output, envelop] = exponential_envelop(y, amp1, amp2)
+    [row, col] = size(y); % row = 1, col = n    
+    coe = col^(-0.9);
+    func = @(x) exp(coe * x);
+    [output, envelop] = func_envelop(y, amp1, amp2, func);
+end
+
+function [output, envelop] = func_envelop(y, amp1, amp2, func)
+    [row, col] = size(y); % row = 1, col = n
+    [a, b] = func_fit(1, amp1, col, amp2, func);
+    x = 1:1:col;
+    envelop = a * func(x) + b;
+    output = y .* envelop;
+end
+
+function [a, b] = func_fit(x1, y1, x2, y2, func)
+    % 计算指数函数参数
+    a = (y2 - y1) / (func(x2) - func(x1));
+    b = y1 - a * func(x1);
+end
