bf17366485afb1208ae48cb6e00498b9831f5830
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2023-07-19 17:58:25 +0800

    refactor function

--- exer_1_1.m
+++ exer_1_1.m
@@ -13,26 +13,3 @@ beat = [1, 0.5, 0.5, 2, 1, 0.5, 0.5, 2];
 
 first_part = cell2mat(arrayfun(@(x, y) gen_tune(x, y), tone, beat, UniformOutput=false));
 sound(first_part, sample_freq);
-
-function y = gen_tune(tone, beat)
-    [freq, width] = trans_freq_width(tone, beat);
-    y = gen_tune_from_freq_width(freq, width);
-end
-
-function y = gen_tune_from_freq_width(freq, width)
-    global sample_freq;
-    global amp;
-    time_step = sample_freq^(-1);
-    t = 0:time_step:width;
-    y = amp * sin(2 * pi * freq * t);
-end
-
-function [freq, width] = trans_freq_width(tone, beat)
-    global base_tone_freq;
-    global beat_time;
-    global tone_mapping;
-    remain = mod(tone, 7);
-    exponent = tone_mapping(remain) + (tone - remain)/7 * 12;
-    freq = base_tone_freq * (2^((exponent - 1)/12));
-    width = beat * beat_time;
-end
